855f0b76770645086b9089e740d48b59a637b939
kavitha <kavitha@9.79.176.162>
2019-03-07 15:46:11 +0530

    commit

---
+++ jenkins.demo/src/test/java/mavenProject/GoogleTest.java
@@ -0,0 +1,29 @@
+package mavenProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	WebDriver driver;
+  @Test
+  public void LaunchTest() {
+	  String Expected="Google";
+	  String Actual=driver.getTitle();
+	  Assert.assertEquals(Expected,Actual);
+  }
+  @BeforeTest
+	  public void LaunchAppTest() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\IBM_SeliniumWD\\chromedriver_win32\\chromedriver.exe");
+	  driver=new ChromeDriver();
+	  driver.get("http://www.google.com");
+  }
+  @AfterTest
+  public void BrowserCloseTest() {
+	  driver.quit();
+}
+  }
+
